beca9f91a36e9c9cbe90c3b442a488710a70e68f
Carrot_hyeong <rjgjfl90@naver.com>
2023-01-25 17:56:28 +0900

    chatapp 이미지 uuid로 저장되게 utils.py 파일 추가 chat model 설계중

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import User
+from .utils import rename_imagefile_to_uuid
 
-# Create your models here.
+class Server(models.Model):
+    server_name = models.CharField(max_length=15)
+    is_open = models.BooleanField()
+    # True -> 공개 서버, False -> 비공개 서버
+    server_profile = models.ImageField(upload_to=rename_imagefile_to_uuid, default='chats/default.PNG', blank=True, null=True)
+    user = models.ManyToManyField(User, related_name='server_user')
+    # chat_room = models.ManyToManyField()
+    # voice_room = models.ManyToManyField()
+
+class ChatRoom(models.Model):
+    chatroom_name = models.CharField(max_length=15)
+
+class ChatMessages(models.Model):
+    chatroom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name='chatmessages_chatroom')
