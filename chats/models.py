076987466e8c4e76b5dba140353815891b92d844
Carrot_hyeong <rjgjfl90@naver.com>
2023-02-01 22:03:55 +0900

    chat model 수정 및 추가

--- chats/models.py
+++ chats/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 from users.models import User
-from .utils import rename_imagefile_to_uuid
+from .utils import rename_serverimagefile_to_uuid, rename_chatimagefile_to_uuid
+
+class ChatRoom(models.Model):
+    chatroom_name = models.CharField(max_length=15)
+
 
 class Server(models.Model):
     server_name = models.CharField(max_length=15)
     is_open = models.BooleanField()
     # True -> 공개 서버, False -> 비공개 서버
-    server_profile = models.ImageField(upload_to=rename_imagefile_to_uuid, default='chats/default.PNG', blank=True, null=True)
+    server_profile = models.ImageField(upload_to=rename_serverimagefile_to_uuid, default='chats/default.PNG', blank=True, null=True)
     user = models.ManyToManyField(User, related_name='server_user')
-    # chat_room = models.ManyToManyField()
+    chat_room = models.ManyToManyField(ChatRoom, blank=True)
     # voice_room = models.ManyToManyField()
-
-class ChatRoom(models.Model):
-    chatroom_name = models.CharField(max_length=15)
 
 class ChatMessages(models.Model):
     chatroom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name='chatmessages_chatroom')
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    message = models.TextField(blank=True)
+    images = models.ImageField(upload_to=rename_chatimagefile_to_uuid, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
--- chats/utils.py
+++ chats/utils.py
@@ -1,11 +1,20 @@
 import os
 from uuid import uuid4
 
-def rename_imagefile_to_uuid(instance, filename):
-    upload_to = f'chats/'
+def rename_serverimagefile_to_uuid(instance, filename):
+    upload_to = f'servers/'
     ext = filename.split('.')[-1]
     uuid = uuid4().hex
 
     filename = f'server_{instance}_{uuid}.{ext}'
+    
+    return os.path.join(upload_to, filename)
+
+def rename_chatimagefile_to_uuid(instance, filename):
+    upload_to = f'chats/{instance}/'
+    ext = filename.split('.')[-1]
+    uuid = uuid4().hex
+
+    filename = f'{uuid}.{ext}'
     
     return os.path.join(upload_to, filename)
