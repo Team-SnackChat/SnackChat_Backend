e78d20b92f6f10ab1d6ef4c24e2ea856630beef5
Carrot_hyeong <rjgjfl90@naver.com>
2023-02-02 01:42:53 +0900

    commit

--- snackchat/settings.py
+++ snackchat/settings.py
@@ -222,7 +222,11 @@ CORS_ALLOW_HEADERS = list(default_headers) + ['Set-Cookie']
 # 나중에 프론트 배포하면 넣어줄 domain 목록
 CORS_ALLOWED_ORIGINS = CORS_ORIGIN_WHITELIST
 # csrf 허용 도메인 목록
-CSRF_TRUSTED_ORIGINS = CORS_ORIGIN_WHITELIST
+CSRF_TRUSTED_ORIGINS = [
+    'http://www.carrotww.shop',
+    'https://www.carrotww.shop',
+    'http://carrotww.shop',
+    'https://carrotww.shop']
 
 CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'DELETE']
 
