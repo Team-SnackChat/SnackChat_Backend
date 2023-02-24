100595bcbbcde2405bd86b22ab2a21bdbcee91a6
Carrot_hyeong <rjgjfl90@naver.com>
2023-02-24 19:14:21 +0900

    model 변경 롤백

--- chats/models.py
+++ chats/models.py
@@ -14,7 +14,7 @@ class Server(models.Model):
     is_open = models.BooleanField(default=False)
     # True -> 공개 서버, False -> 비공개 서버
     server_profile = models.ImageField(upload_to=rename_serverimagefile_to_uuid, default='chats/default.PNG', blank=True, null=True)
-    user = models.ManyToManyField(User, blank=True)
+    user = models.ManyToManyField(User, related_name='server_user', blank=True)
     chat_room = models.ManyToManyField(ChatRoom, related_name='server_chat_room', blank=True)
     # chat_room = models.ManyToManyField(ChatRoom, through='server_chatroom_mediate', related_name='server_chat_room', blank=True)
     # voice_room = models.ManyToManyField()
